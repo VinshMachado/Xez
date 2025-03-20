da0beac65e982b1d90197f3b2305f6272b70232e
Sion Aaron Lobo <sionaaronlobo@gmail.com>
2025-03-20 21:39:14 +0530

    made the page with the card area is responsive for phone not ipad ppl fuk them

--- portfolio/app/Components/Box.js
+++ portfolio/app/Components/Box.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Box = () => {
-  return <div>Box</div>;
+  const det = ["Work", "Services", "About", "Plans"];
+
+  return (
+    <div className="flex justify-center space-x-4 flex-wrap space-y-4 ">
+      {det.map((item, index) => (
+        <div
+          key={index}
+          className="bg-slate-400 rounded-2xl w-44 h-52 flex justify-center items-center"
+        >
+          <div className=" flex-col justify-center items-center">
+            <div className="bg-purple-400 w-24 h-24 rounded-lg"></div>
+            <span className="pt-4 text-black font-bold text-lg text-center flex justify-center">
+              {item}
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Box;
--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import React from "react";
+import Box from "./Components/Box";
 
-export default function Home() {
-  return <div>hello world</div>;
-}
+const page = () => {
+  return (
+    <div className="bg-neutral-500 sm:h-screen flex justify-center items-center">
+      <div className="bg-gray-300 w-screen sm:h-[93vh]    rounded-2xl m-10 sm:m-x-4flex flex-col">
+        <div className="flex justify-center text-3xl pt-2">CodeX</div>
+        <div className="flex justify-center text-3xl pt-40 font-bold text-center">
+          Design and Development Partner For{" "}
+        </div>
+        <div className="flex justify-center text-3xl pt-1 pb-16 text-gray-400 font-bold  text-center">
+          Product-led Founders
+        </div>
+
+        <Box />
+        <div className="flex justify-center sm:pt-10 pt-15 pb-10  ">
+          <button className="bg-black text-white rounded-3xl w-36 h-12 ">
+            Get in touch
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
