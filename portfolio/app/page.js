d9f149fe3e194aa18a6430eea52a3a52375bb481
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-03-21 20:46:18 +0530

    some things

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -4,12 +4,12 @@ import Box from "./Components/Box";
 const page = () => {
   return (
     <div className="bg-neutral-100 sm:h-screen flex justify-center items-center">
-      <div className="bg-white  w-screen sm:h-[93vh]  justify-center items-center w-full rounded-2xl m-10 sm:m-x-4flex flex-col shadow-xl">
+      <div className="bg-white  w-screen sm:h-[93vh]  justify-center items-center w-full rounded-2xl sm:m-10 sm:m-x-4flex flex-col shadow-xl">
         <div className="flex justify-center text-3xl mt-10 ">
           <b>Code</b>
           <b className="text-purple-600 text-3xl">X</b>
         </div>
-        <div className="flex justify-center sm:text-4xl mt-10 font-bold text-center">
+        <div className="flex justify-center font-extrabold sm:text-4xl text-xl mt-10 sm:font-bold text-center">
           Design and Development Partner For{" "}
         </div>
         <div className="flex justify-center  sm:text-3xl pt-1 pb-16 text-gray-500  font-bold  text-center">
