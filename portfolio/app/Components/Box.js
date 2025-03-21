0ac8198cdda9140c5db8ebc1c4b189cbcc615059
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-03-21 21:23:28 +0530

    no changes

--- portfolio/app/Components/Box.js
+++ portfolio/app/Components/Box.js
@@ -8,10 +8,13 @@ const Box = () => {
       {det.map((item, index) => (
         <div
           key={index}
+          onMouseEnter={console.log("hello")}
           className="bg-white hover:w-64 hover:h-80 rounded-2xl w-60 h-72 flex justify-center hover:shadow-purple-300 items-center shadow-gray-400 shadow-lg"
         >
           <div className=" flex-col justify-center items-center">
-            <div className="bg-gray-400 w-24 h-24 rounded-lg"></div>
+            <div className="bg-gray-400 w-24 h-24 rounded-lg ">
+              <img src="./rocket.gif"></img>
+            </div>
             <span className="pt-4 text-black font-bold text-2xl text-center flex justify-center">
               {item}
             </span>
